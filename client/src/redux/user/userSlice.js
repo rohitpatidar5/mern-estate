5966cc107dfd34c3a1c7b5d8b30fe9fabf1854a1
Rohit Patidar <rohitpatidar583@gmail.com>
2024-07-18 16:15:52 +0530

    add sign out user functionality

--- client/src/redux/user/userSlice.js
+++ client/src/redux/user/userSlice.js
@@ -1,11 +1,9 @@
 import { createSlice } from '@reduxjs/toolkit';
-
 const initialState = {
   currentUser: null,
   error: null,
   loading: false,
 };
-
 const userSlice = createSlice({
   name: 'user',
   initialState,
@@ -31,63 +29,49 @@ const userSlice = createSlice({
       state.error = null;
     },
     updateUserFailure: (state, action) => {
-
       state.error = action.payload;
-
       state.loading = false;
-
     },
-
     deleteUserStart: (state) => {
-
       state.loading = true;
-
     },
-
     deleteUserSuccess: (state) => {
-
       state.currentUser = null;
-
       state.loading = false;
-
       state.error = null;
-
     },
-
     deleteUserFailure: (state, action) => {
-
       state.error = action.payload;
-
       state.loading = false;
-
     },
-
+    signOutUserStart: (state) => {
+      state.loading = true;
+    },
+    signOutUserSuccess: (state) => {
+      state.currentUser = null;
+      state.loading = false;
+      state.error = null;
+    },
+    signOutUserFailure: (state, action) => {
+      state.error = action.payload;
+      state.loading = false;
+    },
   },
-
 });
-
-
 
 export const {
   signInStart,
   signInSuccess,
   signInFailure,
-
   updateUserFailure,
-
   updateUserSuccess,
-
   updateUserStart,
-
   deleteUserFailure,
-
   deleteUserSuccess,
-
   deleteUserStart,
-
+  signOutUserFailure,
+  signOutUserSuccess,
+  signOutUserStart,
 } = userSlice.actions;
-
-
-
 
 export default userSlice.reducer;
