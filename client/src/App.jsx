18148ac5a0b20138fbbbcd27234a51df8394e783
Rohit Patidar <rohitpatidar583@gmail.com>
2024-07-18 23:54:06 +0530

    fix bug

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
+import Home from './pages/Home.jsx';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import About from './pages/About';
