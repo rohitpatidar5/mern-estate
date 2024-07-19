c14603281ae04e58195aeff41df552d07fdfbc14
Rohit Patidar <rohitpatidar583@gmail.com>
2024-07-19 10:53:42 +0530

    fix error

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -4,6 +4,18 @@ import { errorHandler } from '../utils/error.js';
 import jwt from 'jsonwebtoken';
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
+
+  if(                                         //if all the fields are empty so we throw a custom error
+    !username || 
+    !email || 
+    !password || 
+    username === '' || 
+    email === '' || 
+    password === ''
+    ){
+    next(errorHandler(400, 'All fields are required'));
+  };
+
   const hashedPassword = bcryptjs.hashSync(password, 10);
   const newUser = new User({ username, email, password: hashedPassword });
   try {
